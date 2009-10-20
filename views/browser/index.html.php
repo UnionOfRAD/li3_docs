053c13ff2b3e52d2051ff9b41204b6d6a39b4ce0
Jon Adams <jon@spacialeffect.com>
2009-10-20 01:56:07 -0700

    Css file created, modified views slightly and applied general lithium network layout (will probably change)

--- views/browser/index.html.php
+++ views/browser/index.html.php
@@ -1,4 +1,5 @@
-<ul>
+<h2>Select a library below:</h2>
+<ul class="libraries">
 	<?php foreach ($libraries as $name => $config) { ?>
 		<?php $display = ucwords(str_replace('_', ' ', $name)); ?>
 		<li><a href="<?php echo $name; ?>"><?php echo $display; ?></a></li>
