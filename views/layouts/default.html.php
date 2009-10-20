ba4c608a07b2ab9e5021fc0bd55b03bb61cec22c
Nate Abele <nate@cakephp.org>
2009-10-20 05:00:43 -0400

    Cleaning up layout code.

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -3,13 +3,12 @@
 	<title><?=$this->title(); ?></title>
 	<?=@$this->html->script('http://thechaw.com/js/jquery-1.3.1.min.js'); ?>
 	<?=@$this->html->script('http://thechaw.com/js/gshowdown.min.js'); ?>
-	<?=@$this->scripts(); ?>
 	<script type="text/javascript">
 		var converter = new Showdown.converter("/");
 
 		$(document).ready(function(){
 			$(".wiki-text").each(function () {
-				$(this).html(converter.makeHtml(jQuery.trim($(this).text())));
+				$(this).html(converter.makeHtml($.trim($(this).text())));
 			});
 		});
 	</script>
