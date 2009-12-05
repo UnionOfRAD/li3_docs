be061a5cc17e96069b77a760dcbd4bfd226f8d73
David Persson <davidpersson@gmx.de>
2009-12-05 18:32:59 +0100

    Adding wrapper for the good old footer.

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -16,6 +16,20 @@
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
 <body class="docs">
+<div id="wrapper">
+	<div id="cli">
+		<div id="cli-display"></div>
+		<div>
+			<form id="cli-form" onSubmit="return false">
+				<input type="text" id="cli-input" />
+				<input id="cli-submit" type="submit" />
+			</form>
+		</div>
+	</div>
+	<div id="git-shortcuts">
+		<span id="git-clone-path" class="clone fixed">git clone code@rad-dev.org:lithium.git</span>
+		<a href="#" id="git-copy" class="copy" title="Copy the git clone shortcut to your clipboard">copy to clipboard</a>
+	</div>
 	<div id="container">
 		<div id="header">
 			<h1><?php echo $this->html->link('Docs', '/docs');?></h1>
@@ -38,17 +52,25 @@
 			<?php echo $this->content; ?>
 		</div>
 	</div>
-<?php
-	$jQuery = 'http://code.jquery.com/jquery-1.4.1.min.js';
-	if (\lithium\core\Environment::is('development')) {
-		$jQuery = '/li3_docs/js/jquery-1.4.1.min';
-	}
- 	echo $this->html->script(array(
-		$jQuery, '/li3_docs/js/showdown.min'
-	));
-?>
+	<div id="footer-spacer"></div>
+</div>
+<div id="footer">
+	<p class="copyright">Pretty much everything is &copy; 2009 and beyond, the Union of Rad</p>
+</div>
+<?php echo $this->html->script(array(
+	'http://code.jquery.com/jquery-1.4.1.min.js',
+	'li3',
+	'cli',
+//		'libraries/ZeroClipboard/ZeroClipboard.js',
+	'showdown.min.js'
+)); ?>
 <script type="text/javascript" charset="utf-8">
 	$(document).ready(function () {
+		li3.setup({
+			base : '<?php echo $this->_request->env('base');?>',
+			testimonials: <?php echo !empty($testimonials) ? 'true' : 'false'; ?>
+		});
+		li3Cli.setup();
 		var converter = new Showdown.converter("/");
 
 		$(".markdown").each(function () {
