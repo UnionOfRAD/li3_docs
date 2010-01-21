891850d482c19b2b78ae45eb80cf38d12b32e25c
David Persson <davidpersson@gmx.de>
2010-01-21 19:30:03 +0100

    Renaming asset.

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -11,7 +11,7 @@
 <head>
 	<?php echo $this->html->charset(); ?>
 	<title>Lithium API <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('base', 'li3.docs')); ?>
+	<?php echo $this->html->style(array('base', 'li3_docs')); ?>
 	<?php echo $this->scripts(); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
