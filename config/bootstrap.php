21c791b009551de07568894633959878e9acd38c
gwoo <gwoohoo@gmail.com>
2010-01-22 16:48:44 -0800

    adding translation message filter to bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,4 +9,12 @@
 // 	)
 // ));
 
+use \lithium\http\Media;
+use \lithium\g11n\Message;
+
+Media::applyFilter('_handle', function($self, $params, $chain) {
+	$params['handler'] += array('outputFilters' => array());
+	$params['handler']['outputFilters'] += Message::contentFilters();
+	return $chain->next($self, $params, $chain);
+});
 ?>
