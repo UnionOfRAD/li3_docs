07bae9cdb2a0b9180ee0318e5bc078fd7f435974
Nate Abele <nate.abele@gmail.com>
2010-08-10 08:14:00 -0400

    Implementing `'showApi'` flag in library configuration.

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,14 @@
 <?php
 
-use \lithium\net\http\Router;
+use lithium\core\Libraries;
+use lithium\net\http\Router;
 // use \app\extensions\route\Locale;
+
+$config = Libraries::get('li3_docs');
+
+if (isset($config['showApi']) && $config['showApi'] === false) {
+	return;
+}
 
 Router::connect('/docs', array('library' => 'li3_docs', 'controller' => 'browser'));
 Router::connect('/docs/{:lib}/{:args}', array(
