5759b92c6588dedc8201b6a2e264129710fb3ca4
Jon Adams <jon@spacialeffect.com>
2010-11-07 19:25:48 -0800

    wrapped links with section element

--- views/elements/links.html.php
+++ views/elements/links.html.php
@@ -1,14 +1,18 @@
 <?php if (isset($object['tags']['link'])) { ?>
+<div id="links" class="section">
+	<section>
 	<h3><?=$t('Links', array('scope' => 'li3_docs')); ?></h3>
-	<ul class="links">
-		<?php foreach ((array) $object['tags']['link'] as $url) { ?>
-			<?php
-				$title = $url;
-				if (strpos($url, ' ')) {
-					list($url, $title) = array_map('trim', explode(' ', $url, 2));
-				}
-			?>
-			<li><?=$this->html->link($title, $url); ?></li>
-		<?php } ?>
-	</ul>
+		<ul class="links">
+			<?php foreach ((array) $object['tags']['link'] as $url) { ?>
+				<?php
+					$title = $url;
+					if (strpos($url, ' ')) {
+						list($url, $title) = array_map('trim', explode(' ', $url, 2));
+					}
+				?>
+				<li><?=$this->html->link($title, $url); ?></li>
+			<?php } ?>
+		</ul>
+	</section>
+</div>
 <?php } ?>
