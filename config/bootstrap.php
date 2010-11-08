c99f2395d8101601c268c9cb2a410b492bc71955
Nate Abele <nate.abele@gmail.com>
2010-11-07 21:46:22 -0500

    Replacing media filter with route for serving static assets through the plugin.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -15,29 +15,4 @@ if (file_exists(LITHIUM_APP_PATH . '/config/bootstrap/g11n.php')) {
 	require_once LITHIUM_APP_PATH . '/config/bootstrap/g11n.php';
 }
 
-/**
- * Filter to serve the assets from plugins.
- */
-use \lithium\action\Dispatcher;
-use \lithium\core\Libraries;
-use \lithium\net\http\Media;
-
-Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
-	list($plugin, $asset) = explode('/', ltrim($params['request']->url, '/'), 2) + array("", "");
-
-	if ($asset && ($library = Libraries::get($plugin)) && isset($library['path'])) {
-		$asset = "{$library['path']}/webroot/{$asset}";
-
-		if (file_exists($asset)) {
-			return function () use ($asset) {
-				$info = pathinfo($asset);
-				$type = Media::type($info['extension']);
-				header("Content-type: {$type['content']}");
-				return file_get_contents($asset);
-			};
-		}
-	}
-	return $chain->next($self, $params, $chain);
-});
-
 ?>
--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,36 @@
 <?php
 
-use \lithium\net\http\Router;
-use \li3_docs\extensions\route\Locale;
+use lithium\action\Response;
+use lithium\net\http\Router;
+use li3_docs\extensions\route\Locale;
 
 Router::connect('/docs', array('library' => 'li3_docs', 'controller' => 'api_browser'));
+
 Router::connect('/docs/{:lib}/{:args}', array(
 	'library' => 'li3_docs', 'controller' => 'api_browser', 'action' => 'view'
 ));
+
+Router::connect('/li3_docs/{:path:js|css}/{:file}.{:type}', array(), function($request) {
+	$req = $request->params;
+	$file = dirname(__DIR__) . "/webroot/{$req['path']}/{$req['file']}.{$req['type']}";
+
+	if (!file_exists($file)) {
+		return;
+	}
+
+	return new Response(array(
+		'body' => file_get_contents($file),
+		'headers' => array('Content-type' => str_replace(
+			array('css', 'js'), array('text/css', 'text/javascript'), $req['type']
+		))
+	));
+});
 
 Router::connect(new Locale(array(
 	'template' => '/docs',
 	'params' => array('library' => 'li3_docs', 'controller' => 'api_browser')
 )));
+
 Router::connect(new Locale(array(
 	'template' => '/docs/{:lib}/{:args}',
 	'params' => array('library' => 'li3_docs', 'controller' => 'api_browser', 'action' => 'view')
