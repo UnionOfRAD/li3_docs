9fbd5a8b2a656ff91fba7af89f5060dadc406849
John Anderson <anderson.johnd@gmail.com>
2012-02-25 18:36:59 -0700

    Stubbing in AJAX action and route.

---
+++ controllers/ApiSearchController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_docs\controllers;
+
+/**
+ * This is the Lithium API search controller. Once the symbol harvester has run,
+ * this class queries that database for quick, ranked API search results.
+ */
+class ApiSearchController extends \lithium\action\Controller {
+
+	/**
+	 * AJAX response action for search autocomplete/results.
+	 *
+	 * @return array Returns an array detailing the symbol and its metadata.
+	 */
+	public function query() {
+		$this->_render['type'] = 'json';
+	}
+}
+
+?>
