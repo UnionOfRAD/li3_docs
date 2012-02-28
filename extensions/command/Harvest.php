5cb228dffa66b47b7ce2188503c6f127ea747cc3
John Anderson <anderson.johnd@gmail.com>
2012-02-27 18:33:23 -0700

    Finishing initial version of the harvester.

--- extensions/command/Harvest.php
+++ extensions/command/Harvest.php
@@ -36,25 +36,100 @@ class Harvest extends \lithium\console\Command {
 	 */
 	public function run() {
 		$this->_readyTables();
-		
 		$extractor = $this->_classes['extractor'];
-		foreach(Libraries::get() as $library => $info) {
+		$libraries = Libraries::get();
+		foreach($libraries as $library => $info) {
+			$this->header($library);
 			$libFiles = Libraries::find($library, array('recursive' => true));
 			foreach($libFiles as $file) {
+				if(preg_match('/mocks|tests|libraries/', $file)) {
+					continue;
+				}
+				$this->out("\n" . $file);
 				$fileData = $extractor::get($library, $file);
-				die('<pre>' . print_r($fileData, true) . '</pre>');
+				$this->_harvestMethods($fileData);
+				$this->_harvestProperties($fileData);
+				$this->_harvestClass($fileData);
 			}
+			$this->out("\n\n");
+		}
+		$this->out("\n\n");
+	}
+	
+	/**
+	 * Clears the harvest database.
+	 *
+	 * @return void
+	 */
+	public function clear() {
+		$this->_readyTables();
+	}
+	
+	/**
+	 * Harvests method meta data from the specified file.
+	 *
+	 * @param string $fileData 
+	 * @return void
+	 */
+	protected function _harvestMethods($fileData) {
+		foreach($fileData['methods'] as $method) {
+			$symbol = Symbols::create();
+			$symbol->name = $method->name;
+			$symbol->type = 'method';
+			$symbol->class = $fileData['identifier'];
+			$symbol->description = $method->getDocComment();
+			$symbol->save();
+			echo '.';
 		}
 	}
 	
+	/**
+	 * Harvests class property data from the specified file.
+	 *
+	 * @param string $fileData 
+	 * @return void
+	 */
+	protected function _harvestProperties($fileData) {
+		foreach($fileData['properties'] as $property) {
+			$symbol = Symbols::create();
+			$symbol->name = $property['name'];
+			$symbol->type = 'property';
+			$symbol->class = $fileData['identifier'];
+			$symbol->description = $property['docComment'];
+			$symbol->save();
+			echo '.';
+		}
+	}
+	
+	/**
+	 * Harvests class information from the specified file.
+	 *
+	 * @param string $fileData 
+	 * @return void
+	 */
+	protected function _harvestClass($fileData) {
+		$symbol = Symbols::create();
+		$symbol->name = array_pop(explode("\\", $fileData['identifier']));
+		$symbol->type = 'class';
+		$symbol->class = $fileData['identifier'];
+		$symbol->description = $fileData['description'];
+		$symbol->save();
+		echo '.';
+	}
+	
+	/**
+	 * Drops, then re-creates the Sqlite3 tables used for searching.
+	 *
+	 * @return void
+	 */
 	protected function _readyTables() {
 		$dropSql = 'DROP TABLE IF EXISTS `symbols`';
 		Symbols::connection()->invokeMethod('_execute', array($dropSql));
 		
 		$createSql = 'CREATE TABLE IF NOT EXISTS `symbols` (
 			`id` INTEGER NOT NULL PRIMARY KEY,
-			`symbol` VARCHAR (255) NOT NULL,
-			`symbol_type` VARCHAR (255) NOT NULL,
+			`name` VARCHAR (255) NOT NULL,
+			`type` VARCHAR (255) NOT NULL,
 			`class` TEXT NOT NULL,
 			`description` TEXT NOT NULL
 		);';
